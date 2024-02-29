fecdaef0a142f392c9f7b303d2484dfc99c1bd4f
CatherineBC <catherinehuangbc@gmail.com>
2024-02-29 19:03:18 +0700

    Exercise in class

--- app/src/main/java/com/ubaya/adv160421082week2/MainFragment.kt
+++ app/src/main/java/com/ubaya/adv160421082week2/MainFragment.kt
@@ -5,14 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ubaya.adv160421082week2.databinding.ActivityMainBinding
+import com.ubaya.adv160421082week2.databinding.FragmentMain2Binding
 
 
 class MainFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main2, container, false)
+    private lateinit var binding: FragmentMain2Binding
+    override fun onCreateView( inflater: LayoutInflater, container:
+    ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentMain2Binding.inflate(
+            inflater,
+            container, false
+        )
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnStart.setOnClickListener {
+            val playerName = binding.txtNama.text.toString()
+            val action = MainFragmentDirections.actionGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+
+
+
 }
